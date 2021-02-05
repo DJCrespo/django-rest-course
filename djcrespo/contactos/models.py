bf2e6101c2a1e67bfffee90b8ec4f1d84004337a
Didier Crespo <dj.crespo.castilla@gmail.com>
2021-02-04 23:08:51 -0600

    Exercises part.1

--- djcrespo/contactos/models.py
+++ djcrespo/contactos/models.py
@@ -5,6 +5,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=10)
     email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- djcrespo/contactos/tests.py
+++ djcrespo/contactos/tests.py
@@ -2,8 +2,60 @@ from django.test import TestCase
 from .models import Contact
 
 # Create your tests here.
-class TestCase(TestCase):
+class PostTest(TestCase):
 
-    def setup(self):
+    def setUp(self):    #10 examples of ORM Create
+        #1
         instance = Contact.objects.create(name='Didier', phone='9992350960', email='dj.crespo.castilla@gmail.com')
         instance.save()
+        #2
+        instance = Contact.objects.create(name='Jesus', phone='', email='')
+        instance.save()
+        #3
+        name = 'Gabriela'
+        phone = '9991275756'
+        email = ''
+        instance = Contact.objects.create(name=name, phone=phone, email=email)
+        instance.save()
+        #4
+        name2 = input('name: ')
+        phone2 = input('phone: ')
+        instance = Contact.objects.create(name=name2, phone=phone2)
+        #5
+        instance = Contact.objects.create(name='Max', phone='9992009901', email='dj.crespo.castilla@hotmail.com')
+        instance.save()
+        #6
+        name3 = input("nombre: ")
+        phone3 = input("telefono: ")
+        email3 = input("email: ")
+        instance = Contact.objects.create(name=name3, phone=phone3, email=email3)
+        #7
+        instance.name = 'Didier Crespo'
+        instance.phone = '9992350960'
+        instance.email = ''
+        instance.save()
+        #8
+
+
+    def test_for_contact(self): 
+        #10 examples of ORM List/Filter/Order
+        #1
+        print(Contact.objects.all())
+        #2
+        print(Contact.objects.filter(phone = ''))
+        #3
+        print(Contact.objects.filter(phone='9992195158'))
+        #4
+        print(Contact.objects.get(pk=2))
+        #5
+        query = Contact.objects.all()
+        print(query.order_by('name'))
+        #6
+        query = Contact.objects.all()
+        print(query.order_by('-name'))
+        #7
+        print(Contact.objects.filter(phone='9992350960').order_by('-name'))
+        #8
+        print(Contact.objects.get(pk=3))
+
+        #10 examples of ORM Get
