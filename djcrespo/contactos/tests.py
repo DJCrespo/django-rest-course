6cd12ff1fa9e8129b73feb02e7664fc284753883
Didier Crespo <dj.crespo.castilla@gmail.com>
2021-02-04 19:44:20 -0600

    contactos's changes migrated

--- djcrespo/contactos/tests.py
+++ djcrespo/contactos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Contact
 
 # Create your tests here.
+class TestCase(TestCase):
+
+    def setup(self):
+        instance = Contact.objects.create(name='Didier', phone='9992350960', email='dj.crespo.castilla@gmail.com')
+        instance.save()
