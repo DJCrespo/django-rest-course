a7d7dc3ac2bcc3cae98f8fc8d6090ab47dd27a9a
Didier Crespo <dj.crespo.castilla@gmail.com>
2021-02-07 11:49:22 -0600

    General changes

--- djcrespo/contactos/views.py
+++ djcrespo/contactos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+#pasa, en este caso, un html
+#def home(request):
+#    return render(request, 'index.html', {'foo':'bar'})
+
+#pasa un json
+def home(request):
+    return JsonResponse({'foo':'bar'})
